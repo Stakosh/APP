97c7813e8e1313e2f7ff4f275098e747a97654bf
Javiera Soto <soto.javi1@gmail.com>
2024-04-23 13:34:01 -0400

    cambiando cosas fevochi por cosas de la u, arreglando contendores

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Inicio from './components/Inicio';
-import Equipos from './components/Equipos';
+import Layout from './components/Layout.js';
+import Inicio from './components/Inicio.js';
+import Noticias from './components/Noticias';
+
 
 function App() {
   return (
@@ -11,12 +12,12 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Inicio />} />
-            <Route path="/equipos" element={<Equipos />} />
+            <Route path="/noticias" element={<Noticias />} />
           </Routes>
         </Layout>
       </div>
     </Router>
   );
-}
+ }
 
 export default App;
--- services/frontend/src/components/Layout.js
+++ services/frontend/src/components/Layout.js
@@ -1,9 +1,10 @@
 // Layout.js
 import React from 'react';
 import logo from '../img/logo.jpg';
+//import fondo2 from  '../img/foto-fondo2.jpg';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Image } from 'react-bootstrap';
-const Calendario = 'Calendario';
+import { FaInstagram, FaFacebook, FaYoutube, FaTiktok, FaTwitter } from 'react-icons/fa';
 
 
 
@@ -18,21 +19,11 @@ const Layout = ({ children }) => {
          <Navbar.Toggle aria-controls="basic-navbar-nav" />
          <Navbar.Collapse id="basic-navbar-nav">
            <Nav className='ml-auto'>
-             <Link to="/equipos" className='nav-link'>
-               Equipos
-             </Link>
-             <a
-                href="https://www.fevochi.cl/wp-content/uploads/2023/10/Calendario-Fevochi-2023-6.pdf"
-                target="_blank"
-                rel="noopener noreferrer"
-                className='nav-link'>
-                  {Calendario}
+             <a href="https://www.fevochi.cl/wp-content/uploads/2023/10/Calendario-Fevochi-2023-6.pdf" target="_blank" className='nav-link'>
+               Justificaciones
              </a>
              <Link to="/noticias" className='nav-link'>
-               Noticias
-             </Link>
-             <Link to="/galeria" className='nav-link'>
-               Galería
+               Proximos Cursos
              </Link>
              <a href="#contacto" className='nav-link'>
                Contacto
@@ -50,7 +41,40 @@ const Layout = ({ children }) => {
 
 
 
-
+     <footer id="contacto" style={{ backgroundColor: '#033D87', color: 'white', padding: '20px 0' }}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>Acerca de UAI</h4>
+            <p>
+              La Universidad Adolfo Ibáñez (UAI) es una universidad privada de
+              excelencia en Chile. Es reconocida por su calidad educativa y por su
+              contribución a la investigación y el desarrollo.
+            </p>
+          </div>
+          <div className="col-md-6">
+            <div className="row">
+              <div className="col-md-6">
+                <h4>Contacto</h4>
+                <p>Correo: info@uai.cl</p>
+                <p>Teléfono: +56 2 23313100</p>
+                <p>Dirección: Diagonal Las Torres 2640, Peñalolén, Santiago, Chile</p>
+              </div>
+              <div className="col-md-6">
+                <h4>Redes Sociales</h4>
+                <div className="social-icons">
+                  <a href="https://www.instagram.com/uai_oficial/" target="_blank" rel="noopener noreferrer"><FaInstagram style={{ color: 'white', marginRight: '10px' }} /></a>
+                  <a href="https://www.facebook.com/UAI.Oficial/" target="_blank" rel="noopener noreferrer"><FaFacebook style={{ color: 'white', marginRight: '10px' }} /></a>
+                  <a href="https://www.youtube.com/user/universidadai" target="_blank" rel="noopener noreferrer"><FaYoutube style={{ color: 'white', marginRight: '10px' }} /></a>
+                  <a href="https://www.tiktok.com/@uai" target="_blank" rel="noopener noreferrer"><FaTiktok style={{ color: 'white', marginRight: '10px' }} /></a>
+                  <a href="https://twitter.com/uai_oficial" target="_blank" rel="noopener noreferrer"><FaTwitter style={{ color: 'white' }} /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     </footer>
    </div>
  );
 };
